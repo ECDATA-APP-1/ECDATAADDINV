3bbd38255b9d10ebe8f58a3aef7ef7d1365aebae
ecdata <ecdatasolution@gmail.com>
2023-06-09 16:30:09 +0800

    002

--- ecinventory/src/utils/qrCodeUtils.ts
+++ ecinventory/src/utils/qrCodeUtils.ts
@@ -1,16 +1,14 @@
-// utils/qrCodeUtils.ts
+import qrcode from 'qrcode';
 
-import QRCode from 'qrcode';
+// Generate a unique QR code for an item
+export const generateUniqueQRCode = async (itemId: number): Promise<string> => {
+  const qrCodeData = `item_${itemId}`; // Unique identifier for the item, e.g., item ID
 
-export const generateQRCode = (item: Item) => {
-  const data = JSON.stringify(item);
-  QRCode.toDataURL(data, (err, url) => {
-    if (err) {
-      console.error('Error generating QR code:', err);
-      return;
-    }
-    console.log('QR code generated:', url);
-    // You can save or use the QR code URL as needed
-    // For example, you can display the QR code in your app using an <Image> component
-  });
+  try {
+    const qrCode = await qrcode.toDataURL(qrCodeData);
+    return qrCode;
+  } catch (error) {
+    console.error('Error generating QR code:', error);
+    throw error;
+  }
 };
